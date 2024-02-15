cb29f6f1e2cfe7589becae4504d4937279b54b6d
adrrf <adrromflo@alum.us.es>
2024-02-15 19:15:44 +0100

    fix: config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap()],
-});
+  site: 'https://ispp-2324-ocial.github.io',
+})
