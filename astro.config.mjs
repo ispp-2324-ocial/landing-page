5102f66c9fb531b7ab5f3f53f14ebdd08e52a939
adrrf <adrromflo@alum.us.es>
2024-02-17 13:32:07 +0100

    fix: added integrations in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://ispp-2324-ocial.github.io',
   base: '/landing-page',
+  integrations: [tailwind(), mdx(), sitemap()]
 })
