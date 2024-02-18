26320bf033f95088722b7bf2eb82f5aeb4e6d7b9
adrrf <adrromflo@alum.us.es>
2024-02-18 18:43:22 +0100

    chore: changed colors of button and link

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,11 @@ module.exports = {
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "ocial-dark-blue": "#0e4781",
+        "ocial-blue": "#3e80d7",
+        "ocial-light-blue": "#aacfff",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
