277c1063190488c9797d037904f4ec11cf85d20a
Fernando Fernández <ferferga@hotmail.com>
2024-04-07 10:03:35 +0000

    refactor: removed dead code, migrate to astr-icon v1, formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -6,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: 'https://ocial.es',
   base: '/',
-  integrations: [tailwind(), mdx(), sitemap()]
+  integrations: [tailwind(), mdx(), sitemap(), icon()]
 })
